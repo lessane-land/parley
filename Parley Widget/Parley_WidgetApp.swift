bd1f6a8222d0f1f5c78223a1f0377999de7826c3
Vanesa Morales <lessane.vanesa@gmail.com>
2026-06-08 17:59:40 +0200

    New widget

---
+++ Parley Widget/Parley_WidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Parley_WidgetApp.swift
+//  Parley Widget
+//
+//  Created by Vanesa Morales on 08/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct Parley_WidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
